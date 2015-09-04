d6e23e2d0f802759e5c38417df326b1fad195134
LongkerDandy <longkerdandy@gmail.com>
2015-09-04 16:47:40 +0800

    add publish auth method

--- mithril-mqtt-api/src/main/java/com/github/longkerdandy/mithril/mqtt/api/Authenticator.java
+++ mithril-mqtt-api/src/main/java/com/github/longkerdandy/mithril/mqtt/api/Authenticator.java
@@ -11,9 +11,20 @@ public interface Authenticator {
      * Authorize client CONNECT
      *
      * @param clientId Client Id
-     * @param userName User Name provided in CONNECT
-     * @param password Password provided in CONNECT
+     * @param userName User Name
+     * @param password Password
      * @return Authorize Result
      */
     CompletableFuture<AuthorizeResult> authConnect(String clientId, String userName, String password);
+
+    /**
+     * Authorize client PUBLISH
+     *
+     * @param clientId  Client Id
+     * @param userName  User Name
+     * @param topicName Topic Name
+     * @param qos       QoS Level
+     * @return Authorize Result
+     */
+    CompletableFuture<AuthorizeResult> authPublish(String clientId, String userName, String topicName, String qos);
 }
