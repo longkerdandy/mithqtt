ecab21beaf5c1fdbae57398cbf473b75e12448d3
LongkerDandy <longkerdandy@gmail.com>
2015-10-08 17:28:14 +0800

    Add UNSUBSCRIBE handler logic

--- mithril-mqtt-broker/src/main/java/com/github/longkerdandy/mithril/mqtt/broker/handler/AsyncRedisHandler.java
+++ mithril-mqtt-broker/src/main/java/com/github/longkerdandy/mithril/mqtt/broker/handler/AsyncRedisHandler.java
@@ -710,7 +710,42 @@ public class AsyncRedisHandler extends SimpleChannelInboundHandler<MqttMessage> 
     }
 
     protected void onUnsubscribe(ChannelHandlerContext ctx, MqttUnsubscribeMessage msg) {
+        if (!this.connected) {
+            logger.trace("Protocol violation: Client {} must first sent a CONNECT message, now received UNSUBSCRIBE message, disconnect the client", this.clientId);
+            ctx.close();
+            return;
+        }
 
+        int packetId = msg.variableHeader().messageId();
+
+        // The Topic Filters (whether they contain wildcards or not) supplied in an UNSUBSCRIBE packet MUST be
+        // compared character-by-character with the current set of Topic Filters held by the Server for the Client. If
+        // any filter matches exactly then its owning Subscription is deleted, otherwise no additional processing
+        // occurs
+        // If a Server deletes a Subscription:
+        // It MUST stop adding any new messages for delivery to the Client.
+        //1 It MUST complete the delivery of any QoS 1 or QoS 2 messages which it has started to send to
+        // the Client.
+        // It MAY continue to deliver any existing messages buffered for delivery to the Client.
+        msg.payload().topics().forEach(topic -> this.redis.removeSubscription(this.clientId, Topics.sanitize(topic)));
+
+        // The Server MUST respond to an UNSUBSUBCRIBE request by sending an UNSUBACK packet. The
+        // UNSUBACK Packet MUST have the same Packet Identifier as the UNSUBSCRIBE Packet.
+        // Even where no Topic Subscriptions are deleted, the Server MUST respond with an
+        // UNSUBACK.
+        // If a Server receives an UNSUBSCRIBE packet that contains multiple Topic Filters it MUST handle that
+        // packet as if it had received a sequence of multiple UNSUBSCRIBE packets, except that it sends just one
+        // UNSUBACK response.
+        logger.trace("Response: Send UNSUBACK back to client {}", this.clientId);
+        this.registry.sendMessage(
+                ctx,
+                MqttMessageFactory.newMessage(
+                        new MqttFixedHeader(MqttMessageType.UNSUBACK, false, MqttQoS.AT_MOST_ONCE, false, 0),
+                        MqttMessageIdVariableHeader.from(packetId),
+                        null),
+                this.clientId,
+                packetId,
+                true);
     }
 
     protected void onPingReq(ChannelHandlerContext ctx, MqttMessage msg) {
